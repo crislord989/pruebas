bfec140c522d857537fac7367e1e9b72bfbc3cde
crislord989 <crislord1050@gmail.com>
2026-02-17 15:57:45 +0000

    Ahora sí: agregando archivos de lógica fuera del entorno

---
+++ primos.py
@@ -0,0 +1,15 @@
+def es_primo(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    
+    return True
+    
+print(es_primo(7))
---
+++ prueba.py
@@ -0,0 +1,13 @@
+from primos import es_primo
+
+def test_numero_primo():
+    assert es_primo(7) == True
+    assert es_primo(13) == True
+    assert es_primo(2) == True
+
+def test_numero_no_primo():
+    assert es_primo(10) == False
+    assert es_primo(4) == False
+    assert es_primo(1) == False
+    assert es_primo(0) == False
+    assert es_primo(-5) == False
